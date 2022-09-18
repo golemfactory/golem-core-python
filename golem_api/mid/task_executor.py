08f32df5e51bf964aa31edefd03aa3941103eff1
Jan Betley <jan.betley@golem.network>
2022-09-18 11:53:37 +0200

    TaskExecutor with concurrent tasks

--- golem_api/mid/task_executor.py
+++ golem_api/mid/task_executor.py
@@ -1,5 +1,6 @@
+import asyncio
 import inspect
-from typing import AsyncIterator, Awaitable, Callable, Iterable, TypeVar, Union
+from typing import AsyncIterator, Awaitable, Callable, Iterable, List, TypeVar, Union
 
 from golem_api.low import Activity
 
@@ -18,11 +19,28 @@ class TaskExecutor:
         self.activity_stream = activity_stream
         self.task_data = task_data
 
+        self._task_data_stream_exhausted = False
+        self._result_queue: asyncio.Queue[TaskResult] = asyncio.Queue()
+
+        #   TODO: remove finished tasks?
+        self._current_tasks: List[asyncio.Task] = []
+        self._main_task: asyncio.Task = asyncio.create_task(self._main_task())
+
     async def results(self) -> AsyncIterator[TaskResult]:
+        while not (self._task_data_stream_exhausted and all(task.done() for task in self._current_tasks)):
+            result = await self._result_queue.get()
+            yield result
+
+    async def _main_task(self):
         for task_data in self.task_data:
-            activity = await self._get_activity()
-            result = await self.execute_task(activity, task_data)
-            yield result  # type: ignore  # mypy, why?
+            task = asyncio.create_task(self._process_single_task(task_data))
+            self._current_tasks.append(task)
+        self._task_data_stream_exhausted = True
+
+    async def _process_single_task(self, task_data: TaskData):
+        activity = await self._get_activity()
+        result = await self.execute_task(activity, task_data)
+        self._result_queue.put_nowait(result)
 
     async def _get_activity(self) -> Activity:
         activity = await self.activity_stream.__anext__()
