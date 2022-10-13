a7b022a6ad6bc67706d340400e507f563a67ea86
Jan Betley <jan.betley@golem.network>
2022-10-13 14:29:51 +0200

    separate mid-level yacat

--- yacat.py
+++ yacat.py
@@ -1,109 +1,61 @@
 import asyncio
-import hashlib
-import queue
-from typing import List
-import random
-import string
 
-from golem_api import execute_tasks, Payload
-from golem_api.commands import Run
-from golem_api.low import Activity
+from golem_api import GolemNode
+from golem_api.default_logger import DefaultLogger
+from golem_api.high.execute_tasks import default_prepare_activity
+from golem_api.mid import (
+    Buffer, Chain, Map, Zip,
+    ActivityPool,
+    default_negotiate, default_create_agreement, default_create_activity,
+)
 
-PAYLOAD = Payload.from_image_hash("055911c811e56da4d75ffc928361a78ed13077933ffa8320fb1ec2db")
-PASSWORD_LENGTH = 4
-CHUNK_SIZE = 2 ** 16
-MAX_WORKERS = 10
-
-tasks_queue = queue.LifoQueue()
-results = set()
+from yacat_no_business_logic import PAYLOAD, MAX_WORKERS, main_task_source, tasks_queue
 
 
-class MainTask:
-    def __init__(self, mask: str, hash_: str, hash_type: int, attack_mode: int):
-        self.mask = mask
-        self.hash_ = hash_
-        self.hash_type = hash_type
-        self.attack_mode = attack_mode
-
-    async def execute(self, activity: Activity):
-        cmd = f"hashcat --keyspace -a {self.attack_mode} -m {self.hash_type} {self.mask}"
-        batch = await activity.execute_commands(Run(cmd))
-        await batch.wait(timeout=30)
-        result = batch.events[-1].stdout
-        keyspace = int(result.strip())
-
-        for skip in range(0, keyspace, CHUNK_SIZE):
-            task = AttackPartTask(
-                mask=self.mask,
-                hash_=self.hash_,
-                hash_type=self.hash_type,
-                attack_mode=self.attack_mode,
-                skip=skip,
-                limit=skip + CHUNK_SIZE,
-            )
-            tasks_queue.put_nowait(task)
-
-
-class AttackPartTask:
-    def __init__(self, mask: str, hash_: str, hash_type: int, attack_mode: int, skip: int, limit: int):
-        self.mask = mask
-        self.hash_ = hash_
-        self.hash_type = hash_type
-        self.attack_mode = attack_mode
-        self.skip = skip
-        self.limit = limit
-
-    async def execute(self, activity: Activity):
-        batch = await activity.execute_commands(*self._commands())
-        await batch.wait(timeout=120)
-        result = batch.events[-1].stdout
-        if result is not None:
-            print(f"FOUND {result.strip()} between {self.skip} and {self.limit}")
-            results.add(result)
-
-    def _commands(self) -> List[Run]:
-        out_fname = "/golem/output/out.potfile"
-        str_cmds = [
-            f"rm -f {out_fname}",
-            f"touch {out_fname}",
-            (
-                f"hashcat -a {self.attack_mode} -m {self.hash_type} "
-                f"--self-test-disable --potfile-disable "
-                f"--skip={self.skip} --limit={self.limit} -o {out_fname} "
-                f"'{self.hash_}' '{self.mask}' "
-                f"|| true"
-            ),
-            f"cat {out_fname}",
-        ]
-        return [Run(str_cmd) for str_cmd in str_cmds]
-
-
-async def main_task_source():
-    mask = "".join(["?a" for _ in range(PASSWORD_LENGTH)])
-    chars = string.ascii_letters + string.digits + string.punctuation
-
+async def async_task_stream():
     while True:
-        if tasks_queue.qsize() < MAX_WORKERS * 2:
-            password = "".join([random.choice(chars) for i in range(len(mask) // 2)])
-            hash_ = hashlib.sha256(password.encode()).hexdigest()
-            task = MainTask(mask=mask, hash_=hash_, hash_type=1400, attack_mode=3)
-            tasks_queue.put(task)
-        else:
-            await asyncio.sleep(0.1)
+        yield await tasks_queue.get()
+
+
+async def filter_proposals(proposal_stream):
+    async for proposal in proposal_stream:
+        print(proposal)
+        yield proposal
+
+
+async def close_agreement_repeat_task(func, args, e):
+    activity, task = args
+    tasks_queue.put_nowait(task)
+    await activity.destroy()
+    await activity.parent.terminate()
 
 
 async def main() -> None:
     asyncio.create_task(main_task_source())
-    async for result in execute_tasks(
-        budget=1,
-        execute_task=lambda activity, task: task.execute(activity),
-        task_data=iter(tasks_queue.get, None),
-        payload=PAYLOAD,
-        max_workers=MAX_WORKERS,
-    ):
-        pass
 
-    print("DONE")
+    golem = GolemNode()
+    golem.event_bus.listen(DefaultLogger().on_event)
+
+    async with golem:
+        allocation = await golem.create_allocation(amount=1)
+        demand = await golem.create_demand(PAYLOAD, allocations=[allocation])
+
+        async for result in Chain(
+            demand.initial_proposals(),
+            filter_proposals,
+            Map(default_negotiate),
+            Map(default_create_agreement),
+            Map(default_create_activity),
+            Map(default_prepare_activity),
+            ActivityPool(max_size=MAX_WORKERS),
+            Zip(async_task_stream()),
+            Map(
+                lambda activity, task: task.execute(activity),
+                on_exception=close_agreement_repeat_task,
+            ),
+            Buffer(size=MAX_WORKERS + 1),
+        ):
+            pass
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
