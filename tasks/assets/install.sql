ece7613c08c35d5024652da06ec4f61a815aa1eb
Jan Betley <jan.betley@golem.network>
2022-11-28 18:25:05 +0100

    minor DB improvements

--- tasks/assets/install.sql
+++ tasks/assets/install.sql
@@ -35,4 +35,53 @@ CREATE TABLE batch (
 );
 
 
+CREATE FUNCTION tasks.demands(run_id text) RETURNS TABLE (demand_id text)
+LANGUAGE SQL
+AS $fff$
+    SELECT  id
+    FROM    tasks.demand
+    WHERE   run_id = $1;
+$fff$
+;
+
+CREATE FUNCTION tasks.proposals(run_id text) RETURNS TABLE (demand_id text, proposal_id text)
+LANGUAGE SQL
+AS $fff$
+    SELECT  d.demand_id, p.id
+    FROM    tasks.proposal      p
+    NATURAL
+    JOIN    tasks.demands($1)   d
+$fff$
+;
+
+CREATE FUNCTION tasks.agreements(run_id text) RETURNS TABLE (demand_id text, proposal_id text, agreement_id text)
+LANGUAGE SQL
+AS $fff$
+    SELECT  p.demand_id, p.proposal_id, a.id
+    FROM    tasks.agreement     a
+    NATURAL
+    JOIN    tasks.proposals($1) p
+$fff$
+;
+
+CREATE FUNCTION tasks.activities(run_id text) RETURNS TABLE (demand_id text, proposal_id text, agreement_id text, activity_id text)
+LANGUAGE SQL
+AS $fff$
+    SELECT  ag.demand_id, ag.proposal_id, ag.agreement_id, ac.id
+    FROM    tasks.activity          ac
+    NATURAL
+    JOIN    tasks.agreements($1)    ag
+$fff$
+;
+
+CREATE FUNCTION tasks.batches(run_id text) RETURNS TABLE (demand_id text, proposal_id text, agreement_id text, activity_id text, batch_id text)
+LANGUAGE SQL
+AS $fff$
+    SELECT  a.demand_id, a.proposal_id, a.agreement_id, a.activity_id, b.id
+    FROM    tasks.batch          b
+    NATURAL
+    JOIN    tasks.activities($1) a
+$fff$
+;
+
 COMMIT;
--- tasks/save_new_objects.py
+++ tasks/save_new_objects.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from golem_api.low import Demand, Proposal, Agreement, Activity, PoolingBatch
+from golem_api.low import Demand, Agreement, Activity, PoolingBatch
 from golem_api.events import NewResource
 
 
@@ -10,17 +10,17 @@ async def save_new_objects(golem, db):
         #         because the order of the inserts **must** be preserved (because of the foreign keys).
         #         This doesn't matter that much now, but might matter more after
         #         https://github.com/golemfactory/golem-api-python/issues/3
-        #   (but also: millions of proposals should not slow down inserting informations about agreements, batches etc
-        #    --> this might be a TODO)
         resource = event.resource
         resource_id = resource.id
 
         if isinstance(event.resource, Demand):
             db.execute("INSERT INTO demand (id, run_id) VALUES (%s, %s)", (resource_id, golem.app_session_id))
-        elif isinstance(event.resource, Proposal):
-            db.execute("INSERT INTO proposal (id, demand_id) VALUES (%s, %s)", (resource_id, resource.demand.id))
         elif isinstance(event.resource, Agreement):
-            db.execute("INSERT INTO agreement (id, proposal_id) VALUES (%s, %s)", (resource_id, resource.parent.id))
+            proposal = resource.parent
+            db.execute("""
+                INSERT INTO proposal (id, demand_id) VALUES (%(proposal_id)s, %(demand_id)s);
+                INSERT INTO agreement (id, proposal_id) VALUES (%(agreement_id)s, %(proposal_id)s);
+            """, {"proposal_id": proposal.id, "demand_id": proposal.demand.id, "agreement_id": resource_id})
         elif isinstance(event.resource, Activity):
             db.execute("INSERT INTO activity (id, agreement_id) VALUES (%s, %s)", (resource_id, resource.parent.id))
         elif isinstance(event.resource, PoolingBatch):
@@ -29,7 +29,7 @@ async def save_new_objects(golem, db):
     golem.event_bus.resource_listen(
         save_resource,
         event_classes=[NewResource],
-        resource_classes=[Demand, Proposal, Agreement, Activity, PoolingBatch],
+        resource_classes=[Demand, Agreement, Activity, PoolingBatch],
     )
 
     await asyncio.Future()
