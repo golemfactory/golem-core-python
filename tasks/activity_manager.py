bab299c66a9fe8aa82b245ab531a80aa43772698
Jan Betley <jan.betley@golem.network>
2022-12-12 13:36:11 +0100

    refactor 2/2

--- tasks/activity_manager.py
+++ tasks/activity_manager.py
@@ -1,10 +1,77 @@
+import asyncio
+
+from golem_core.mid import (
+    Chain, Map,
+    default_negotiate, default_create_agreement, default_create_activity, default_prepare_activity,
+)
+
 class ActivityManager:
-    def __init__(self, golem, db, *, payload, max_activities):
+    def __init__(self, golem, db, *, payload, max_activities, max_concurrent=2):
         self.golem = golem
         self.db = db
         self.payload = payload
         self.max_activities = max_activities
+        self.max_concurrent = max_concurrent
 
     async def run(self):
-        from tasks.create_activities import create_activities
-        await create_activities(self.golem, self.db, self.payload, self.max_activities)
+        chain = await self._get_chain()
+        tasks = []
+
+        semaphore = asyncio.BoundedSemaphore(self.max_concurrent)
+        chain_lock = asyncio.Lock()
+        while True:
+            await semaphore.acquire()
+
+            running_activity_cnt = await self._get_running_activity_cnt()
+            running_task_cnt = len([task for task in tasks if not task.done()])
+
+            if running_activity_cnt + running_task_cnt < self.max_activities:
+                tasks.append(asyncio.create_task(self._get_new_activity(chain_lock, chain, semaphore)))
+            else:
+                semaphore.release()
+                await asyncio.sleep(1)
+
+    async def _get_running_activity_cnt(self):
+        #   Q: why don't we use golem.all_resources(Activity) here?
+        #   A: because:
+        #   *   this works without changes after a restart
+        #   *   database contents are higher quality than temporary object states
+        #       (e.g. we might have some other entity that changes the database only)
+        data = await self.db.select("""
+            SELECT  count(*)
+            FROM    activities(%s) run_act
+            JOIN    activity       all_act
+                ON  run_act.activity_id = all_act.id
+            WHERE   all_act.status != 'CLOSED'
+        """, (self.golem.app_session_id,))
+        return data[0][0]
+
+    async def _prepare_activity(self, activity):
+        await default_prepare_activity(activity)
+        await self.db.aexecute("""
+            UPDATE  activity
+            SET     status = 'READY'
+            WHERE   id = %s
+        """, (activity.id,))
+        return activity
+
+    async def _get_chain(self):
+        allocation = await self.golem.create_allocation(amount=1)
+        demand = await self.golem.create_demand(self.payload, allocations=[allocation])
+
+        chain = Chain(
+            demand.initial_proposals(),
+            Map(default_negotiate),
+            Map(default_create_agreement),
+            Map(default_create_activity),
+            Map(self._prepare_activity),
+        )
+        return chain
+
+    async def _get_new_activity(self, chain_lock, chain, semaphore):
+        try:
+            async with chain_lock:
+                activity_awaitable = await chain.__anext__()
+            await activity_awaitable
+        finally:
+            semaphore.release()
--- tasks/task_executor.py
+++ tasks/task_executor.py
@@ -1,3 +1,9 @@
+import asyncio
+
+from golem_core.mid import (
+    Chain, Map, Zip, Buffer,
+)
+
 class TaskExecutor:
     def __init__(self, golem, db, *, get_tasks, max_concurrent):
         self.golem = golem
@@ -5,6 +11,55 @@ class TaskExecutor:
         self.get_tasks = get_tasks
         self.max_concurrent = max_concurrent
 
+        self.locked_activities = []
+
     async def run(self):
-        from tasks.process_tasks import process_tasks
-        await process_tasks(self.golem, self.db, self.get_tasks, self.max_concurrent)
+        async for _ in Chain(
+            self._activity_stream(),
+            Zip(self._task_stream()),
+            Map(self._process_task),
+            Buffer(size=self.max_concurrent),
+        ):
+            pass
+
+    async def _task_stream(self):
+        for task in self.get_tasks():
+            yield task
+
+    async def _activity_stream(self):
+        while True:
+            next_activity_id = await self._get_next_activity_id()
+            if next_activity_id is not None:
+                yield self.golem.activity(next_activity_id)
+            else:
+                await asyncio.sleep(0.1)
+
+    async def _process_task(self, activity, task):
+        try:
+            print(f"Task start: {activity}")
+            result = await task(activity)
+            return result
+        finally:
+            self.locked_activities.remove(activity.id)
+
+    async def _get_next_activity_id(self):
+        locked_activities = self.locked_activities
+
+        data = await self.db.select("""
+            SELECT  activity_id
+            FROM    tasks.activities(%(app_session_id)s) run_act
+            JOIN    activity                             all_act
+                ON  run_act.activity_id = all_act.id
+            WHERE   all_act.status = 'READY'
+                AND NOT activity_id = ANY(%(locked_activities)s)
+            LIMIT   1
+        """, {"app_session_id": self.golem.app_session_id, "locked_activities": locked_activities})
+        if data:
+            activity_id = data[0][0]
+            if activity_id in locked_activities:
+                #   Rare case where we performed two selects concurrently
+                #   (I'm not 100% sure if this is possible at all)
+                return None
+            locked_activities.append(activity_id)
+            return activity_id
+        return None
