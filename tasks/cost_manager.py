8790d07a245db6222dd3ca23099e4d85cae560fe
Jan Betley <jan.betley@golem.network>
2022-12-27 18:46:16 +0100

    CostManager fully working

--- tasks/cost_manager.py
+++ tasks/cost_manager.py
@@ -24,13 +24,10 @@ class CostManager:
         await self._wait_for_debit_note_write(debit_note)
 
         activity_id = (await debit_note.get_data()).activity_id
-        try:
-            if await self._activity_old_enough(activity_id):
-                result_price = await self._activity_result_price(activity_id)
-                if result_price is not None and result_price > self.result_max_price:
-                    await self._stop_activity(activity_id)
-        except Exception as e:
-            print(e)
+        if await self._activity_old_enough(activity_id):
+            result_price = await self._activity_result_price(activity_id)
+            if result_price is not None and result_price > self.result_max_price:
+                await self._stop_activity(activity_id)
 
     async def _wait_for_debit_note_write(self, debit_note):
         #   Debit note might not yet be saved to the database, and cost aggregates
@@ -54,7 +51,48 @@ class CostManager:
         """, {"activity_id": activity_id, "interval": f"{self.init_seconds} seconds"}))
 
     async def _activity_result_price(self, activity_id):
-        return 7
+        results_data = await self.db.select("""
+            SELECT  cnt
+            FROM    tasks.results
+            WHERE   run_id = %(run_id)s
+            ORDER BY created_ts DESC
+            LIMIT 1
+        """)
+        if not results_data or not results_data[0][0]:
+            #   No results yet -> we don't know the cost --> we don't evaluate.
+            #   This can be considered a TODO - what if we have only not-really-working
+            #   activities so we'll always have 0 results?
+            return None
+
+        results = results_data[0][0]
+
+        result_price_data = await self.db.select("""
+            WITH
+            activity_cost AS (
+                SELECT  max(amount) AS amount
+                FROM    tasks.debit_note
+                WHERE   activity_id = %(activity_id)s
+            ),
+            activity_results AS (
+                WITH
+                activity_batches AS (
+                    SELECT  count(*)    AS cnt
+                    FROM    tasks.batch
+                    WHERE   activity_id = %(activity_id)s
+                ),
+                all_batches AS (
+                    SELECT  count(*) AS cnt
+                    FROM    tasks.batches(%(run_id)s)
+                )
+                SELECT  %(results)s * (activity_batches.cnt / all_batches.cnt::numeric) AS results
+                FROM    activity_batches, all_batches
+            )
+            SELECT  amount / (results + 0.000000001)
+            FROM    activity_cost, activity_results
+        """, {"activity_id": activity_id, "results": results})
+
+        result_price = result_price_data[0][0]
+        return result_price
 
     async def _stop_activity(self, activity_id):
         await self.db.aexecute("""
