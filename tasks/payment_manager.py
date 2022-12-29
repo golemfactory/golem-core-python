058ee4a3b94e82fe959976ccaa11b561baafb329
Jan Betley <jan.betley@golem.network>
2022-12-29 14:23:26 +0100

    hourly budget works again

--- tasks/payment_manager.py
+++ tasks/payment_manager.py
@@ -1,19 +1,21 @@
 import asyncio
 from datetime import datetime, timedelta
-
-from ya_payment.exceptions import ApiException
+from decimal import Decimal
 
 from golem_core.events import NewResource
 from golem_core.low import Agreement, DebitNote, Invoice
 
 class BudgetExhausted(Exception):
-    def __init__(self, allocation):
-        self.allocation = allocation
-        super().__init__(f"Couldn't accept a debit note - {allocation} is exhausted")
+    def __init__(self, hourly_budget):
+        self.hourly_budget = hourly_budget
+
+        msg = f"Couldn't accept a debit note - got debit notes for more than {hourly_budget} in the last hour"
+        super().__init__(msg)
 
 class PaymentManager:
     def __init__(self, golem, db, budget_str):
         self.golem = golem
+        self.db = db
 
         #   NOTE: Now we have a hardcoded logic "budget is specified hourly".
         #         This might ofc change in the future, this logic is contained in PaymentManager fully.
@@ -46,8 +48,7 @@ class PaymentManager:
 
         while True:
             if self._budget_exahusted:
-                await self._allocation.get_data(force=True)
-                raise BudgetExhausted(self._allocation)
+                raise BudgetExhausted(self.hourly_budget)
 
             #   Recreate allocation every hour
             if (datetime.now() - self._allocation_created_ts).seconds > 3600:
@@ -60,23 +61,86 @@ class PaymentManager:
         self._allocation = await self.golem.create_allocation(amount=self.hourly_budget)
         self._allocation_created_ts = datetime.now()
 
-    def _allocation_too_old(self):
-        return False
-
     async def _save_agreement(self, event):
         agreement_id = event.resource.id
         if agreement_id not in self._agreement_has_invoice:
             self._agreement_has_invoice[agreement_id] = False
 
     async def _process_debit_note(self, event):
+        #   Q: Why not just try to accept the debit note and react to failure?
+        #   A: Because yagna doesn't enforce the limit. You can use allocation for amount X to accept
+        #      any number of debit notes, as long as each of them is below X.
+        #      (X is decreased with every accepted **invoice**, but not debit note)
+        #
+        #   This is **probably** a `yagna` bug (waiting for an answer). Allocation-based implementation
+        #   would be much prettier and we'll probably want to go towards it.
+        #
+        #   Q: Why not gather "activity_id -> amount" map in-memory?
+        #   A: Because this logic should be preserved on restarts.
         debit_note = event.resource
+        await debit_note.get_data()
+
         try:
-            await debit_note.accept_full(self._allocation)
-        except ApiException as e:
-            if e.status == 400 and "Not enough funds" in str(e):
+            last_hour_amount = await self._last_hour_amount(debit_note)
+
+            if last_hour_amount > self.hourly_budget:
                 self._budget_exahusted = True
+                print(f"Failed to accept {debit_note} because {last_hour_amount} > {self.hourly_budget}")
             else:
-                raise
+                await debit_note.accept_full(self._allocation)
+        except Exception as e:
+            print(e)
+
+    async def _last_hour_amount(self, debit_note):
+        #   TODO: This logic might require additional check (but I still hope we'll not need
+        #         this SQL at all because necessary logic will be in `debit_note.accept_full`.
+        #   NOTE: We have the debit note here (and treat the related activity_id differently) because
+        #         we don't know if this debit note is already in the database or not.
+        activity_id = debit_note.data.activity_id
+
+        data = await self.db.select("""
+            WITH
+            this_run_debit_notes AS (
+                SELECT  d.id,
+                        d.created_ts,
+                        d.activity_id,
+                        d.amount
+                FROM    tasks.debit_note             d
+                JOIN    tasks.activities(%(run_id)s) a
+                    ON  d.activity_id = a.activity_id
+            ),
+            hour_ago_sum AS (
+                WITH
+                activity_max AS (
+                    SELECT  activity_id,
+                            max(amount) AS amount
+                    FROM    this_run_debit_notes
+                    WHERE   created_ts + '1 hour'::interval < now()
+                    GROUP BY 1
+                )
+                SELECT  coalesce(sum(amount), 0) AS amount
+                FROM    activity_max
+            ),
+            sum_without_activity AS (
+                WITH
+                activity_max AS (
+                    SELECT  activity_id,
+                            max(amount) AS amount
+                    FROM    this_run_debit_notes
+                    WHERE   activity_id != %(activity_id)s
+                    GROUP BY 1
+                )
+                SELECT  coalesce(sum(amount), 0) AS amount
+                FROM    activity_max
+            )
+            SELECT  sum_without_activity.amount - hour_ago_sum.amount
+            FROM    sum_without_activity, hour_ago_sum
+        """, {"activity_id": activity_id})
+
+        #   NOTE: data[0][0] might be even negative (e.g. if we have only a single
+        #         activity for more than an hour) - this should still work fine
+
+        return data[0][0] + Decimal(debit_note.data.total_amount_due)
 
     async def _process_invoice(self, event):
         invoice = event.resource
