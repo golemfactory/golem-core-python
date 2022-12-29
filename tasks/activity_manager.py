ccb13462ecca433bf5ae577163d9a5f5f6c37e77
Jan Betley <jan.betley@golem.network>
2022-12-29 16:23:37 +0100

    _get_activity reqorked (still includes big bunch of debug spam)

--- tasks/activity_manager.py
+++ tasks/activity_manager.py
@@ -3,7 +3,6 @@ from datetime import datetime, timezone, timedelta
 
 from golem_core.commands import Deploy, Start
 from golem_core.mid import (
-    Chain, Map, SimpleScorer,
     default_negotiate, default_create_agreement, default_create_activity,
 )
 from golem_core.low.exceptions import BatchError, BatchTimeoutError
@@ -20,7 +19,9 @@ class ActivityManager:
 
         self._current_allocation = None
         self._demand_expiration = None
-        self._chain = None
+        self._demand = None
+        self._initial_proposals = None
+        self._initial_proposals_lock = asyncio.Lock()
         self._tasks = []
 
     async def run(self):
@@ -48,12 +49,77 @@ class ActivityManager:
             running_task_cnt = len([task for task in self._tasks if not task.done()])
 
             if running_activity_cnt + running_task_cnt < self.max_activities:
-                chain = await self._get_chain()
-                activity_awaitable = await chain.__anext__()
-                self._tasks.append(asyncio.create_task(self._get_new_activity(activity_awaitable, semaphore)))
+                self._tasks.append(asyncio.create_task(self._get_new_activity(semaphore)))
             else:
                 semaphore.release()
                 await asyncio.sleep(1)
+
+    async def _get_new_activity(self, semaphore):
+        try:
+            import aiofiles
+            import uuid
+            x = uuid.uuid4()
+            start = datetime.now()
+
+            async def write_log():
+                try:
+                    while True:
+                        await asyncio.sleep(1)
+                        msg = f"{x} runs for {(datetime.now() - start).seconds} \n"
+                        async with aiofiles.open("aaa_" + self.golem.app_session_id, mode='a+') as f:
+                            await f.write(msg)
+                except asyncio.CancelledError:
+                    msg = f"{x} STOP \n"
+                    async with aiofiles.open("aaa_" + self.golem.app_session_id, mode='a+') as f:
+                        await f.write(msg)
+                    raise
+
+            task = asyncio.create_task(write_log())
+
+            async def negotiate(proposal):
+                return await asyncio.wait_for(default_negotiate(proposal), timeout=10)
+
+            async def create_agreement(proposal):
+                return await asyncio.wait_for(default_create_agreement(proposal), timeout=10)
+
+            async def create_activity(agreement):
+                return await asyncio.wait_for(default_create_activity(agreement), timeout=10)
+
+            async def prepare_activity(activity):
+                return await asyncio.wait_for(self._prepare_activity(activity), timeout=300)
+
+            awaitable = self._get_new_proposal()
+            chain_parts = (negotiate, create_agreement, create_activity, prepare_activity)
+            for chain_part in chain_parts:
+                awaited = await awaitable
+                async with aiofiles.open("aaa_" + self.golem.app_session_id, mode='a+') as f:
+                    await f.write(str(x) + " " + str(awaited) + "\n")
+                awaitable = chain_part(awaited)
+            awaited = await awaitable
+            async with aiofiles.open("aaa_" + self.golem.app_session_id, mode='a+') as f:
+                await f.write(str(x) + " " + str(awaited) + "\n")
+            return awaited
+        finally:
+            task.cancel()
+            semaphore.release()
+
+    async def _get_new_proposal(self):
+        if self._demand is None or self._demand_expires_soon():
+            await self._create_demand()
+        async with self._initial_proposals_lock:
+            return await self._initial_proposals.__anext__()
+
+    def _demand_expires_soon(self):
+        assert self._demand_expiration is not None
+        return (self._demand_expiration - datetime.now(timezone.utc)) < timedelta(seconds=300)
+
+    async def _create_demand(self):
+        allocation = await self._get_allocation()
+        self._demand_expiration = datetime.now(timezone.utc) + timedelta(seconds=1800)
+        self._demand = await self.golem.create_demand(
+            self.payload, allocations=[allocation], expiration=self._demand_expiration
+        )
+        self._initial_proposals = self._demand.initial_proposals()
 
     async def recover(self):
         #   Recoverable
@@ -141,51 +207,9 @@ class ActivityManager:
             await self.db.aexecute(
                 "UPDATE activity SET status = 'READY' WHERE id = %(activity_id)s",
                 {"activity_id": activity.id})
+            return activity
         except Exception:
-            print("DEPLOY/START FAILED")
             await self.db.close_activity(activity, 'deploy/start failed')
-
-    async def _get_chain(self):
-        if self._chain is None or self._demand_expires_soon():
-            self._chain = await self._create_new_chain()
-
-            #   Cancel all current tasks because we don't want to try to create activities using the old chain
-            #   TODO: this might leave some negotiations/preparations/etc in a unexpected state. Is this a problem?
-            for task in self._tasks:
-                task.cancel()
-
-        return self._chain
-
-    def _demand_expires_soon(self):
-        assert self._demand_expiration is not None
-        return (self._demand_expiration - datetime.now(timezone.utc)) < timedelta(seconds=300)
-
-    async def _create_new_chain(self):
-        allocation = await self._get_allocation()
-
-        self._demand_expiration = datetime.now(timezone.utc) + timedelta(seconds=1800)
-        demand = await self.golem.create_demand(
-            self.payload, allocations=[allocation], expiration=self._demand_expiration
-        )
-
-        async def negotiate(proposal):
-            return await asyncio.wait_for(default_negotiate(proposal), timeout=10)
-
-        async def create_agreement(proposal):
-            return await asyncio.wait_for(default_create_agreement(proposal), timeout=10)
-
-        async def create_activity(agreement):
-            return await asyncio.wait_for(default_create_activity(agreement), timeout=10)
-
-        chain = Chain(
-            demand.initial_proposals(),
-            SimpleScorer(self._score_proposal, min_proposals=10),
-            Map(negotiate),
-            Map(create_agreement),
-            Map(create_activity),
-            Map(self._prepare_activity),
-        )
-        return chain
 
     async def _get_allocation(self):
         #   Startup - wait until PaymentManager created an allocation
@@ -195,43 +219,17 @@ class ActivityManager:
             else:
                 return self._current_allocation
 
-    async def _get_new_activity(self, activity_awaitable, semaphore):
-        try:
-            start = datetime.now()
+    # @staticmethod
+    # async def _score_proposal(proposal):
+    #     MAX_LINEAR_COEFFS = [0.001, 0.001, 0]
 
-            import aiofiles
+    #     properties = proposal.data.properties
+    #     if properties['golem.com.pricing.model'] != 'linear':
+    #         return None
 
-            async def write_log():
-                try:
-                    while True:
-                        await asyncio.sleep(1)
-                        msg = f"{id(activity_awaitable)} runs for {(datetime.now() - start).seconds} \n"
-                        async with aiofiles.open("aaa_" + self.golem.app_session_id, mode='a+') as f:
-                            await f.write(msg)
-                except asyncio.CancelledError:
-                    msg = f"{id(activity_awaitable)} STOP \n"
-                    async with aiofiles.open("aaa_" + self.golem.app_session_id, mode='a+') as f:
-                        await f.write(msg)
-                    raise
-
-            task = asyncio.create_task(write_log())
-
-            await activity_awaitable
-        finally:
-            task.cancel()
-            semaphore.release()
-
-    @staticmethod
-    async def _score_proposal(proposal):
-        MAX_LINEAR_COEFFS = [0.001, 0.001, 0]
-
-        properties = proposal.data.properties
-        if properties['golem.com.pricing.model'] != 'linear':
-            return None
-
-        coeffs = properties['golem.com.pricing.model.linear.coeffs']
-        for val, max_val in zip(coeffs, MAX_LINEAR_COEFFS):
-            if val > max_val:
-                return None
-        else:
-            return 1 - (coeffs[0] + coeffs[1])
+    #     coeffs = properties['golem.com.pricing.model.linear.coeffs']
+    #     for val, max_val in zip(coeffs, MAX_LINEAR_COEFFS):
+    #         if val > max_val:
+    #             return None
+    #     else:
+    #         return 1 - (coeffs[0] + coeffs[1])
