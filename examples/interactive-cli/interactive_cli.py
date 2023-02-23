af6af1a8596bef3daaa4a39993de9cfc25f84356
Proxy <proxy.idrin@gmail.com>
2023-02-23 17:45:57 +0100

    step2 done

--- examples/interactive-cli/interactive_cli.py
+++ examples/interactive-cli/interactive_cli.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
 import os
+from dataclasses import dataclass
 from datetime import timedelta
-from typing import Never, Callable, Optional
+from functools import partial
+from typing import Never, Callable, Optional, Coroutine
 
-from asciimatics.effects import Print, Effect
+from asciimatics.effects import Print
 from asciimatics.event import KeyboardEvent
 from asciimatics.exceptions import NextScene, InvalidFields
 from asciimatics.renderers import FigletText, SpeechBubble
@@ -14,16 +16,23 @@ from asciimatics.widgets import Frame, Layout, Label, Text, Button, PopUpDialog,
 from yarl import URL
 
 from golem_core import GolemNode
+from golem_core.low import Allocation
 
 ASCIIMATICS_SCREEN_UPDATE_INTERVAL = timedelta(seconds=1/20)
 
 
+@dataclass
+class DemoModel:
+    golem_node: Optional[GolemNode] = None
+    allocation: Optional[Allocation] = None
+
+
 class BusyPopUpDialog(PopUpDialog):
     def __init__(self, screen, text, task: asyncio.Task, callback: Callable, theme="green"):
-        super().__init__(screen, text, buttons=[], on_close=None, has_shadow=False, theme=theme)
-
         self._task = task
         self._callback = callback
+
+        super().__init__(screen, text, buttons=[], on_close=None, has_shadow=False, theme=theme)
 
     def _update(self, frame_no):
         if self._task.done():
@@ -46,10 +55,37 @@ class BusyPopUpDialog(PopUpDialog):
         self.scene.remove_effect(self)
 
 
+def show_error_pop_up_dialog(frame: Frame, exception: InvalidFields):
+    frame.scene.add_effect(PopUpDialog(
+        frame.screen,
+        "The following fields are invalid:\n\n{}".format(
+            '\n'.join(f"- {frame.find_widget(field).label[:-1]}" for field in exception.fields)
+        ),
+        ["OK"]
+    ))
 
+
+
+def on_frame_submit(frame: Frame, message: str, async_function: Callable[[], Coroutine], callback_function: Callable) -> None:
+    try:
+        frame.save(validate=True)
+    except InvalidFields as e:
+        show_error_pop_up_dialog(frame, e)
+        return
+
+    loop = asyncio.get_event_loop()
+
+    connection_task = loop.create_task(async_function())
+
+    frame.scene.add_effect(
+        BusyPopUpDialog(frame.screen, message, connection_task, callback_function),
+        reset=False,
+    )
 
 class Step0Scene(Scene):
-    def __init__(self, screen: Screen, scenes_count: int):
+    def __init__(self, screen: Screen, scenes_count: int, model: DemoModel):
+        self._model = model
+
         super().__init__(
             effects=[
                 Print(
@@ -84,8 +120,8 @@ class Step0Scene(Scene):
 
 
 class Step1Scene(Scene):
-    def __init__(self, screen: Screen, step_count: int):
-        self._connection_task = None
+    def __init__(self, screen: Screen, step_count: int, model: DemoModel):
+        self._model = model
 
         self._frame = frame = Frame(
             screen=screen,
@@ -111,7 +147,13 @@ class Step1Scene(Scene):
         layout.add_widget(Text('YAGNA_API_URL:', 'yagna_api_url', validator=self._validate_yagna_api_url))
         layout.add_widget(Text('YAGNA_APPKEY:', 'yagna_app_key', validator=self._validate_yagna_appkey))
         layout.add_widget(Divider(draw_line=False))
-        layout.add_widget(Button('Connect', self._on_connect_button_click))
+        layout.add_widget(Button('Connect', on_click=partial(
+            on_frame_submit,
+            frame=frame,
+            message='Connecting...',
+            async_function=self._connect_to_golem_node,
+            callback_function=self._on_connect_to_golem_node_done,
+        )))
 
         frame.fix()
 
@@ -130,31 +172,8 @@ class Step1Scene(Scene):
     def _validate_yagna_appkey(value: str) -> bool:
         return bool(value)
 
-    def _on_connect_button_click(self):
-        try:
-            self._frame.save(validate=True)
-        except InvalidFields as e:
-            self.add_effect(PopUpDialog(
-                self._frame.screen,
-                "The following fields are invalid:\n\n{}".format(
-                    '\n'.join(f"- {self._frame.find_widget(field).label[:-1]}" for field in e.fields)
-                ),
-                ["OK"]
-            ))
-
-            return
-
-        loop = asyncio.get_event_loop()
-
-        connection_task = loop.create_task(self._create_golem_node())
-
-        self.add_effect(
-            BusyPopUpDialog(self._frame.screen, "Connecting...", connection_task, self._on_connect_result),
-            reset=False,
-        )
-
-    async def _create_golem_node(self) -> Optional[Exception]:
-        self._frame.screen._golem_node = golem_node = GolemNode(
+    async def _connect_to_golem_node(self) -> Optional[Exception]:
+        self._model.golem_node = golem_node = GolemNode(
             base_url=self._frame.data['yagna_api_url'],
             app_key=self._frame.data['yagna_app_key'],
         )
@@ -167,8 +186,7 @@ class Step1Scene(Scene):
         except Exception as e:
             return e
 
-
-    def _on_connect_result(self, result_exception: Optional[Exception]):
+    def _on_connect_to_golem_node_done(self, result_exception: Optional[Exception]):
         if result_exception:
             self.add_effect(PopUpDialog(
                 self._frame.screen,
@@ -181,22 +199,85 @@ class Step1Scene(Scene):
 
 
 class Step2Scene(Scene):
-    def __init__(self, screen: Screen, step_count: int):
-        effects = [
-            Frame(
-                screen=screen,
-                height=screen.height,
-                width=screen.width,
-                title=f"[2/{step_count}] Preparing fund allocation",
-                reduce_cpu = True,
-            ),
-        ]
+    def __init__(self, screen: Screen, step_count: int, model: DemoModel):
+        self._model = model
 
-        super().__init__(effects, name="step2")
+        self._frame = frame = Frame(
+            screen=screen,
+            height=10,
+            width=(screen.width * 2) // 3,
+            can_scroll=False,
+            title=f"[1/{step_count}] Preparing fund allocation",
+            reduce_cpu=True,
+            name='connection_details',
+            data={
+                'funds': '1',
+            }
+        )
+
+        layout = Layout([100])
+        frame.add_layout(layout)
+
+        layout.add_widget(Label(
+            'Great! Connection to yagna works, now let\'s declare maximum amount of funds that we want to spend on this demo.',
+            height=2,
+        ))
+        layout.add_widget(Divider(draw_line=False))
+        layout.add_widget(Text('Funds to spend:', 'funds', validator=self._validate_funds))
+        layout.add_widget(Divider(draw_line=False))
+        layout.add_widget(Button('Allocate', on_click=partial(
+            on_frame_submit,
+            frame=frame,
+            message='Allocating...',
+            async_function=self._create_allocation,
+            callback_function=self._on_create_allocation_result,
+        )))
+
+        frame.fix()
+
+        super().__init__([frame], name="step2")
+
+    def _validate_funds(self, value: str) -> bool:
+        try:
+            float(value)
+        except Exception:
+            return False
+
+        return True
+
+    def _on_next_button_click(self):
+        try:
+            self._frame.save(validate=True)
+        except InvalidFields as e:
+            show_error_pop_up_dialog(self._frame, e)
+            return
+
+        loop = asyncio.get_event_loop()
+
+        connection_task = loop.create_task(self._create_allocation())
+
+        self.add_effect(
+            BusyPopUpDialog(self._frame.screen, "Allocating...", connection_task, self._on_create_allocation_result),
+            reset=False,
+        )
+
+    async def _create_allocation(self) -> Optional[Exception]:
+        allocation = await self._model.golem_node.create_allocation(
+            float(self._frame.data['funds'])
+        )
+
+        self._model.allocation = allocation
+
+        return None
+
+    def _on_create_allocation_result(self, result_exception: Optional[Exception]):
+        raise NextScene('step3')
 
 
 class Step3Scene(Scene):
-    def __init__(self, screen: Screen, step_count: int):
+    def __init__(self, screen: Screen, step_count: int, model: DemoModel):
+        self._model = model
+
         effects = [
             Frame(
                 screen=screen,
@@ -322,15 +403,16 @@ async def amain(screen: Screen) -> Never:
         await asyncio.sleep(ASCIIMATICS_SCREEN_UPDATE_INTERVAL.total_seconds())
 
 
-def prepare_scenes(screen: Screen) -> None:
+def prepare_scenes(screen: Screen, model: DemoModel) -> None:
     scenes = [
         Step0Scene,
-        Step1Scene, Step2Scene, Step3Scene, Step4Scene, Step5Scene,
-        Step6Scene, Step7Scene, Step8Scene, Step9Scene, Step10Scene,
+        Step1Scene, Step2Scene, Step3Scene,
+        # Step1Scene, Step2Scene, Step3Scene, Step4Scene, Step5Scene,
+        # Step6Scene, Step7Scene, Step8Scene, Step9Scene, Step10Scene,
     ]
     scenes_count = len(scenes) - 1
 
-    screen.set_scenes([scene(screen, scenes_count) for scene in scenes])
+    screen.set_scenes([scene(screen, scenes_count, model) for scene in scenes])
 
 
 def main():
@@ -338,10 +420,10 @@ def main():
 
     exception = None
     screen = Screen.open()
-    screen._golem_node = None
+    model = DemoModel()
 
     try:
-        prepare_scenes(screen)
+        prepare_scenes(screen, model)
 
         asyncio.run(amain(screen), debug=True)
     except KeyboardInterrupt:
