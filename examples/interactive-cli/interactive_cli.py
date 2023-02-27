77d048a8aa1009c83da10ca5f64126a7f8342083
approxit <approxit@users.noreply.github.com>
2023-02-27 18:08:38 +0100

    step3 done

--- examples/interactive-cli/interactive_cli.py
+++ examples/interactive-cli/interactive_cli.py
@@ -4,7 +4,7 @@ import os
 from dataclasses import dataclass
 from datetime import timedelta
 from functools import partial
-from typing import Never, Callable, Optional, Coroutine
+from typing import Never, Callable, Optional, Coroutine, cast
 
 from asciimatics.effects import Print
 from asciimatics.event import KeyboardEvent
@@ -12,11 +12,12 @@ from asciimatics.exceptions import NextScene, InvalidFields
 from asciimatics.renderers import FigletText, SpeechBubble
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
-from asciimatics.widgets import Frame, Layout, Label, Text, Button, PopUpDialog, Divider
+from asciimatics.widgets import Frame, Layout, Label, Text, Button, PopUpDialog, Divider, DropdownList
 from yarl import URL
 
-from golem_core import GolemNode
-from golem_core.low import Allocation
+from golem_core import GolemNode, Payload
+from golem_core.low import Allocation, Demand
+from yapapi.payload import vm
 
 ASCIIMATICS_SCREEN_UPDATE_INTERVAL = timedelta(seconds=1/20)
 
@@ -25,6 +26,7 @@ ASCIIMATICS_SCREEN_UPDATE_INTERVAL = timedelta(seconds=1/20)
 class DemoModel:
     golem_node: Optional[GolemNode] = None
     allocation: Optional[Allocation] = None
+    demand: Optional[Demand] = None
 
 
 class BusyPopUpDialog(PopUpDialog):
@@ -65,6 +67,21 @@ def show_error_pop_up_dialog(frame: Frame, exception: InvalidFields):
     ))
 
 
+def validate_float(value: str) -> bool:
+    try:
+        float(value)
+    except Exception:
+        return False
+
+    return True
+
+def validate_int(value: str) -> bool:
+    try:
+        int(value)
+    except Exception:
+        return False
+
+    return True
 
 def on_frame_submit(frame: Frame, message: str, async_function: Callable[[], Coroutine], callback_function: Callable) -> None:
     try:
@@ -223,7 +240,7 @@ class Step2Scene(Scene):
             height=2,
         ))
         layout.add_widget(Divider(draw_line=False))
-        layout.add_widget(Text('Funds to spend:', 'funds', validator=self._validate_funds))
+        layout.add_widget(Text('Funds to spend:', 'funds', validator=validate_float))
         layout.add_widget(Divider(draw_line=False))
         layout.add_widget(Button('Allocate', on_click=partial(
             on_frame_submit,
@@ -236,30 +253,6 @@ class Step2Scene(Scene):
         frame.fix()
 
         super().__init__([frame], name="step2")
-
-    def _validate_funds(self, value: str) -> bool:
-        try:
-            float(value)
-        except Exception:
-            return False
-
-        return True
-
-    def _on_next_button_click(self):
-        try:
-            self._frame.save(validate=True)
-        except InvalidFields as e:
-            show_error_pop_up_dialog(self._frame, e)
-            return
-
-        loop = asyncio.get_event_loop()
-
-        connection_task = loop.create_task(self._create_allocation())
-
-        self.add_effect(
-            BusyPopUpDialog(self._frame.screen, "Allocating...", connection_task, self._on_create_allocation_result),
-            reset=False,
-        )
 
     async def _create_allocation(self) -> Optional[Exception]:
         allocation = await self._model.golem_node.create_allocation(
@@ -277,22 +270,78 @@ class Step2Scene(Scene):
 class Step3Scene(Scene):
     def __init__(self, screen: Screen, step_count: int, model: DemoModel):
         self._model = model
+        self._frame = frame = Frame(
+            screen=screen,
+            height=11,
+            width=(screen.width * 2) // 3,
+            title=f"[3/{step_count}] Creation of demand",
+            reduce_cpu = True,
+            data={
+                'image': 'debian',
+                'min_ram': "0.5",
+                'min_cpu': "1",
+                'min_storage': "2",
+            }
+        )
 
-        effects = [
-            Frame(
-                screen=screen,
-                height=screen.height,
-                width=screen.width,
-                title=f"[3/{step_count}] Creation of demand",
-                reduce_cpu = True,
+        layout = Layout([100])
+        frame.add_layout(layout)
+
+        layout.add_widget(Label(
+            'Funds are prepared. Now let\'s describe our needs. In this demo, let\'s narrow down our options to running virtual machine with following values.',
+            height=2,
+        ))
+        layout.add_widget(Divider(draw_line=False))
+        layout.add_widget(DropdownList(
+            label='Linux distro',
+            options=(
+                ('Debian', 'debian'),
+                ('Ubuntu', 'ubuntu'),
+                ('Alpine', 'alpine'),
             ),
-        ]
+            name='image',
+            fit=True,
+        ))
+        layout.add_widget(Text('Minimal RAM (GiBs):', 'min_ram', validator=validate_float))
+        layout.add_widget(Text('Minimal CPUs:', 'min_cpu', validator=validate_int))
+        layout.add_widget(Text('Minimal storage (GiBs):', 'min_storage', validator=validate_float))
+        layout.add_widget(Divider(draw_line=False))
+        layout.add_widget(Button('Publish', on_click=partial(
+            on_frame_submit,
+            frame=frame,
+            message='Publishing...',
+            async_function=self._create_demand,
+            callback_function=self._on_create_demand_result,
+        )))
 
-        super().__init__(effects, name="step3")
+        frame.fix()
 
+        super().__init__([frame], name="step3")
+
+    async def _create_demand(self) -> Optional[Exception]:
+        payload = await vm.repo(
+            image_hash='9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae',
+            min_mem_gib=float(self._frame.data['min_ram']),
+            min_cpu_threads=int(self._frame.data['min_cpu']),
+            min_storage_gib=float(self._frame.data['min_storage']),
+        )
+
+        demand = await self._model.golem_node.create_demand(
+            payload=cast(Payload, payload),
+            allocations=[self._model.allocation]
+        )
+
+        self._model.demand = demand
+
+        return None
+
+    def _on_create_demand_result(self, result_exception: Optional[Exception]):
+        raise NextScene('scene4')
 
 class Step4Scene(Scene):
-    def __init__(self, screen: Screen, step_count: int):
+    def __init__(self, screen: Screen, step_count: int, model: DemoModel):
+        self._model = model
+
         effects = [
             Frame(
                 screen=screen,
@@ -303,7 +352,7 @@ class Step4Scene(Scene):
             ),
         ]
 
-        super().__init__(effects, name="step4")
+        super().__init__(effects, name="scene4")
 
 
 class Step5Scene(Scene):
@@ -406,7 +455,7 @@ async def amain(screen: Screen) -> Never:
 def prepare_scenes(screen: Screen, model: DemoModel) -> None:
     scenes = [
         Step0Scene,
-        Step1Scene, Step2Scene, Step3Scene,
+        Step1Scene, Step2Scene, Step3Scene, Step4Scene,
         # Step1Scene, Step2Scene, Step3Scene, Step4Scene, Step5Scene,
         # Step6Scene, Step7Scene, Step8Scene, Step9Scene, Step10Scene,
     ]
