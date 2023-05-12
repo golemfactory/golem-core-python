4a8d7506b5cad81a118aef388d03de22a350b733
approxit <approxit@users.noreply.github.com>
2023-05-12 08:32:43 +0200

    activity manager flow

--- big_5_draft.py
+++ big_5_draft.py
@@ -5,8 +5,8 @@ class PayAllPaymentManager:
 
         self._allocation = Allocation.create(budget=self._budget)
 
-        event_bus.register('InvoiceReceived', self.on_invoice_received)
-        event_bus.register('DebitNoteReceived', self.on_debit_note_received)
+        event_bus.register(InvoiceReceived(allocation=self._allocation), self.on_invoice_received)
+        event_bus.register(DebitNoteReceived(allocation=self._allocation), self.on_debit_note_received)
 
     def get_allocation(self) -> 'Allocation':
         return self._allocation
@@ -19,7 +19,8 @@ class PayAllPaymentManager:
 
 
 class FifoOfferManager:
-    def __init__(self, get_allocation: 'Callable'):
+    def __init__(self, get_allocation: 'Callable', event_bus):
+        self._event_bus = event_bus
         self._proposals = []
         self.get_allocation = get_allocation
 
@@ -31,7 +32,8 @@ class FifoOfferManager:
         demand_builder.add(allocation)
         demand = demand_builder.create_demand()
         self._proposals = demand.initial_proposals()
-        demand.on_new_proposal(self.on_new_proposal)
+
+        self._event_bus.register(ProposalReceived(demand=demand), self.on_new_proposal)
 
     def on_new_proposal(self, proposal: 'Proposal') -> None:
         self._proposals.append(proposal)
@@ -64,12 +66,9 @@ class FifoAgreementManager:
                 pass
 
 
-class PooledActivityManager:
-    def __init__(self, get_agreement: 'Callable', pool_size: int):
+class SingleUseActivityManager:
+    def __init__(self, get_agreement: 'Callable'):
         self.get_agreement = get_agreement
-        self._pool_size = pool_size
-
-        self.activities = []
 
     def get_activity(self) -> 'Activity':
         while True:
@@ -80,144 +79,165 @@ class PooledActivityManager:
             except Exception:
                 pass
 
-    def do_work(self, tasks: 'Iterable', before_all=None, after_all=None) -> None:
-        activities = [self.get_activity() for _ in range(self._pool_size)]
+    def apply_activity_decorators(self, func, work):
+        if not hasattr(work, '_activity_decorators'):
+            return func
+
+        result = func
+        for dec in work._activity_decorators:
+            result = partial(dec, result)
+
+        return result
 
 
-        if before_all:
-            activity.do(before_all)
+    def do_work(self, work: 'Callable', on_activity_begin: 'Callable' = None, on_activity_end: 'Callable' = None) -> 'WorkResult':
+        activity = self.get_activity()
 
-        for task in tasks:
-            try:
-                activity.do(task)
-            except Exception:
-                pass
+        if on_activity_begin:
+            activity.do(on_activity_begin)
 
-        if after_all:
-            activity.do(after_all)
+        decorated_do = self.apply_activity_decorators(activity.do, work)
 
+        try:
+            return decorated_do(work)
+        except Exception:
+            pass
 
-def blacklist_wrapper(get_offer, blacklist):
-    def wrapper(*args, **kwargs):
-        while True:
-            offer = get_offer()
+        if on_activity_end:
+            activity.do(on_activity_end)
 
-            if offer in blacklist:
-                continue
+    def do_work_list(self, work_list: 'List[Callable]', on_activity_begin: 'Callable' = None, on_activity_end: 'Callable' = None) -> 'List[WorkResult]':
+        results = []
 
-            return offer
+        for work in work_list:
+            results.append(
+                self.do_work(work, on_activity_begin, on_activity_end)
+            )
 
-    return wrapper
-
-def restart_wrapper():
-    pass
+        return results
 
 
-def redundance_wrapper():
-    pass
+def blacklist_offers(blacklist: 'List'):
+    def _blacklist_offers(func):
+        def wrapper(*args, **kwargs):
+            while True:
+                offer = func()
+
+                if offer not in blacklist:
+                    return offer
+
+        return wrapper
+
+    return _blacklist_offers
 
 
-def default_before_all(context):
+def retry(tries: int = 3):
+    def _retry(func):
+        def wrapper(work) -> 'WorkResult':
+            count = 0
+            errors = []
+
+            while count <= tries:
+                try:
+                    return func(work)
+                except Exception as err:
+                    count += 1
+                    errors.append(err)
+
+            raise errors  # List[Exception] to Exception
+
+        return wrapper
+
+    return _retry
+
+
+def redundancy_cancel_others_on_first_done(size: int = 3):
+    def _redundancy(func):
+        def wrapper(work):
+            tasks = [func(work) for _ in range(size)]
+
+            task_done, tasks_in_progress = return_on_first_done(tasks)
+
+            for task in tasks_in_progress:
+                task.cancel()
+
+            return task_done
+
+        return wrapper
+
+    return _redundancy
+
+
+def default_on_begin(context):
     context.deploy()
     context.start()
 
 
+def default_on_end(context):
+    context.terminate()
+
+    # After this function call we should have check for activity termination
+
+
+def activity_decorator(dec, *args, **kwargs):
+    def _activity_decorator(func):
+        if not hasattr(func, '_activity_decorators'):
+            func._activity_decorators = []
+
+        func._activity_decorators.append(dec)
+
+        return func
+
+    return _activity_decorator
+
+
+@activity_decorator(redundancy_cancel_others_on_first_done(size=5))
+@activity_decorator(retry(tries=5))
 def work(context):
     context.run('echo hello world')
 
 
 async def work_service(context):
-    context.run('app start deamon')
+    context.run('app --daemon')
+
+
+async def work_service_fetch(context):
+    context.run('app --daemon &')
 
     while context.run('app check-if-running'):
         sleep(1)
 
 
-def default_after_all(context):
-    context.terminate()
+async def work_batch(context):
+    script = context.create_batch()
+    script.add_run('echo 1')
+    script.add_run('echo 2')
+    script.add_run('echo 3')
+    await script.run()
 
 
 def main():
     payload = RepositoryVmPayload(image_url='...')
     budget = 1.0
-    task_list = [
+    work_list = [
+        work,
+        work,
         work,
     ]
+    event_bus = EventBus()
 
-    payment_manager = PayAllPaymentManager(budget)
-    offer_manager = FifoOfferManager(payment_manager.get_allocation)
+    payment_manager = PayAllPaymentManager(budget, event_bus)
+
+    offer_manager = FifoOfferManager(payment_manager.get_allocation, event_bus)
     offer_manager.collect_proposals_for(payload)
+
     agreement_manager = FifoAgreementManager(
-        blacklist_wrapper(offer_manager.get_offer, ['banned_node_id'])
-    )
-    activity_manager = PooledActivityManager(agreement_manager.get_agreement, pool_size=5)
-    activity_manager.do_work(
-        task_list,
-        before_all=default_before_all,
-        after_all=default_after_all
+        blacklist_offers(['banned_node_id'])(offer_manager.get_offer)
     )
 
-    # Activity per task
-    for task in task_list:
-        with activity_manager.activity() as ctx:
-            default_before_all()
-            ctx.run(task)
-            default_after_all()
-
-    # Single activity
-    with activity_manager.activity_context(
-        before_all=default_before_all,
-        after_all=default_after_all
-    ) as ctx:
-        for task in task_list:
-            ctx.run(task)
-
-    #Activity pool
-    def pool_work(ctx):
-        for task in task_list:
-            yield ctx.run(task)
-
-    activity_manager.activity_pool(
-        before_all_per_activity=default_before_all,
-        after_all_per_activity=default_after_all,
-        work=pool_work,
+    activity_manager = SingleUseActivityManager(agreement_manager.get_agreement)
+    activity_manager.do_work_list(
+        work_list,
+        on_activity_begin=default_on_begin,
+        on_activity_end=default_on_end
     )
-
-    def activity_context():
-        activity = self.get_activity()
-        if before_all:
-            activity.do(before_all)
-
-        yield activity
-
-        if after_all:
-            activity.do(after_all)
-
-    def retry_failed_task_in_new_activity_plugin(get_task: 'Callable', retry_count_per_task: int):
-        activity = get_activity()
-        activity.before_all()
-        task = get_task()
-        tries = retry_count_per_task
-        while True:
-            try:
-                activity.do(task)
-            except Exception as e:
-                if not retry_count_per_task:
-                    raise Exception('Number of tries exceeded!') from e
-
-                retry_count_per_task -= 1
-                activity.after_all()
-                activity = get_activity()
-                activity.before_all()
-            else:
-                task = get_task()
-                tries = retry_count_per_task
-
-        activity.after_all()
-
-    def redundance(get_task: 'Callable', redundance_size: int):
-        task = get_task()
-
-        activities = [get_activity() for _ in range(redundance_size)]
-        for activity in activities:
-            activity.before_all()
