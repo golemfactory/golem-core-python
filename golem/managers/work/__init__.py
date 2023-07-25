7a2457ed90fa892f139e5ccbbfe8d52b2415328b
Lucjan Dudek <lucekdudek@gmail.com>
2023-07-25 15:51:14 +0200

    Add missing imports; enamed pipeline test module

--- golem/managers/work/__init__.py
+++ golem/managers/work/__init__.py
@@ -1,9 +1,11 @@
 from golem.managers.work.asynchronous import AsynchronousWorkManager
+from golem.managers.work.mixins import WorkManagerPluginsMixin
 from golem.managers.work.plugins import redundancy_cancel_others_on_first_done, retry, work_plugin
 from golem.managers.work.sequential import SequentialWorkManager
 
 __all__ = (
     "AsynchronousWorkManager",
+    "WorkManagerPluginsMixin",
     "SequentialWorkManager",
     "work_plugin",
     "redundancy_cancel_others_on_first_done",
