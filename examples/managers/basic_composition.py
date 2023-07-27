ecd510ff93bc591abf1222d66c9ac9e63e56f31b
Lucjan Dudek <lucekdudek@gmail.com>
2023-07-27 15:14:59 +0200

    Simplify basic composition managers example

--- examples/managers/basic_composition.py
+++ examples/managers/basic_composition.py
@@ -1,46 +1,22 @@
 import asyncio
 import logging.config
-from datetime import timedelta
-from random import randint, random
+from random import randint
 from typing import List
 
 from golem.managers import (
     AddChosenPaymentPlatform,
     AutoDemandManager,
-    BlacklistProviderId,
-    LinearAverageCostPricing,
-    MapScore,
     PayAllPaymentManager,
-    PropertyValueLerpScore,
-    RandomScore,
-    RejectIfCostsExceeds,
-    RejectProposal,
     ScoredAheadOfTimeAgreementManager,
     SequentialNegotiationManager,
     SequentialWorkManager,
     SingleUseActivityManager,
     WorkContext,
     WorkResult,
-    redundancy_cancel_others_on_first_done,
-    retry,
-    work_plugin,
 )
 from golem.node import GolemNode
-from golem.payload import RepositoryVmPayload, defaults
-from golem.resources import DemandData, ProposalData
+from golem.payload import RepositoryVmPayload
 from golem.utils.logging import DEFAULT_LOGGING
-
-BLACKLISTED_PROVIDERS = [
-    "0x3b0f605fcb0690458064c10346af0c5f6b7202a5",
-    "0x7ad8ce2f95f69be197d136e308303d2395e68379",
-    "0x40f401ead13eabe677324bf50605c68caabb22c7",
-]
-
-
-async def blacklist_func(demand_data: DemandData, proposal_data: ProposalData) -> None:
-    provider_id = proposal_data.issuer_id
-    if provider_id in BLACKLISTED_PROVIDERS:
-        raise RejectProposal(f"Provider ID `{provider_id}` is blacklisted by the requestor")
 
 
 async def commands_work_example(context: WorkContext) -> str:
@@ -52,7 +28,6 @@ async def commands_work_example(context: WorkContext) -> str:
     return result
 
 
-@work_plugin(redundancy_cancel_others_on_first_done(size=2))
 async def batch_work_example(context: WorkContext):
     if randint(0, 1):
         raise Exception("Random fail")
@@ -77,53 +52,20 @@ async def main():
 
     golem = GolemNode()
 
-    linear_average_cost = LinearAverageCostPricing(
-        average_cpu_load=0.2, average_duration=timedelta(seconds=5)
-    )
-
     payment_manager = PayAllPaymentManager(golem, budget=1.0)
-    demand_manager = AutoDemandManager(
-        golem,
-        payment_manager.get_allocation,
-        payload,
-    )
+    demand_manager = AutoDemandManager(golem, payment_manager.get_allocation, payload)
     negotiation_manager = SequentialNegotiationManager(
         golem,
         demand_manager.get_initial_proposal,
         plugins=[
             AddChosenPaymentPlatform(),
-            # class based plugin
-            BlacklistProviderId(BLACKLISTED_PROVIDERS),
-            RejectIfCostsExceeds(1, linear_average_cost),
-            # func plugin
-            blacklist_func,
-            # lambda plugin
-            lambda _, proposal_data: proposal_data.issuer_id not in BLACKLISTED_PROVIDERS,
-            # lambda plugin with reject reason
-            lambda _, proposal_data: RejectProposal(f"Blacklisting {proposal_data.issuer_id}")
-            if proposal_data.issuer_id in BLACKLISTED_PROVIDERS
-            else None,
         ],
     )
     agreement_manager = ScoredAheadOfTimeAgreementManager(
-        golem,
-        negotiation_manager.get_draft_proposal,
-        plugins=[
-            MapScore(linear_average_cost, normalize=True, normalize_flip=True),
-            [0.5, PropertyValueLerpScore(defaults.INF_MEM, zero_at=1, one_at=8)],
-            [0.1, RandomScore()],
-            [0.0, lambda proposals_data: [random() for _ in range(len(proposals_data))]],
-            [0.0, MapScore(lambda proposal_data: random())],
-        ],
+        golem, negotiation_manager.get_draft_proposal
     )
     activity_manager = SingleUseActivityManager(golem, agreement_manager.get_agreement)
-    work_manager = SequentialWorkManager(
-        golem,
-        activity_manager.do_work,
-        plugins=[
-            retry(tries=5),
-        ],
-    )
+    work_manager = SequentialWorkManager(golem, activity_manager.do_work)
 
     async with golem:
         async with payment_manager, demand_manager, negotiation_manager, agreement_manager:
