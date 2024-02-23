ec09fe7b984d619fd4d222f03744042df6bdd884
approxit <approxit@users.noreply.github.com>
2024-02-23 12:17:22 +0100

    manager rename + fixes

--- golem/managers/demand/aggregating.py
+++ golem/managers/demand/aggregating.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from typing import Awaitable, Callable, MutableMapping, Optional, Sequence
+from typing import Awaitable, Callable, MutableMapping, Sequence
 
 from golem.managers import DemandManager
 from golem.node import GolemNode
@@ -9,7 +9,7 @@ from golem.utils.asyncio import create_task_with_logging, ensure_cancelled_many
 from golem.utils.logging import trace_span
 
 
-class UnionDemandManager(DemandManager):
+class AggregatingDemandManager(DemandManager):
     """DemandManager that combines multiple get_initial_proposal functions into one."""
 
     def __init__(
@@ -26,16 +26,10 @@ class UnionDemandManager(DemandManager):
 
         self._queue: asyncio.Queue[Proposal] = asyncio.Queue()
 
-    @trace_span("Stopping UnionDemandManager", log_level=logging.INFO)
+    @trace_span("Stopping AggregatingDemandManager", log_level=logging.INFO)
     async def stop(self) -> None:
         await ensure_cancelled_many(self._task_map.values())
         self._task_map.clear()
-
-    async def _feed_queue(self, func: Callable[[], Awaitable[Proposal]]):
-        proposal = await func()
-        async with self._lock:
-            self._queue.put_nowait(proposal)
-            del self._task_map[func]
 
     @trace_span("Getting initial proposal", show_results=True)
     async def get_initial_proposal(self) -> Proposal:
@@ -44,17 +38,24 @@ class UnionDemandManager(DemandManager):
         `get_initial_proposal_funcs` will be called concurrently, where not returned results will
         be saved and returned with the next call. If no proposals are saved, and some other
         functions are pending, completed functions will be run again. In case of multiple proposals
-        are saved, they will be returned in declaration order of `get_initial_proposal_funcs`.
+        are saved, they will be returned in completion  order.
         """
 
-        async with self._lock:
-            try:
-                return self._queue.get_nowait()
-            except asyncio.QueueEmpty:
-                pass
+        try:
+            return self._queue.get_nowait()
+        except asyncio.QueueEmpty:
+            pass
 
+        async with self._lock:
             for func in self._get_initial_proposal_funcs:
                 if func not in self._task_map:
                     self._task_map[func] = create_task_with_logging(self._feed_queue(func))
 
         return await self._queue.get()
+
+    async def _feed_queue(self, func: Callable[[], Awaitable[Proposal]]):
+        proposal = await func()
+        self._queue.put_nowait(proposal)
+
+        async with self._lock:
+            del self._task_map[func]
--- tests/unit/test_managers_demand.py
+++ tests/unit/test_managers_demand.py
@@ -2,10 +2,10 @@ import asyncio
 
 import pytest
 
-from golem.managers.demand.union import UnionDemandManager
+from golem.managers.demand.aggregating import AggregatingDemandManager
 
 
-async def test_union_demand_manager_should_block_until_proposal_arrives(mocker):
+async def test_aggregating_demand_manager_should_block_until_proposal_arrives(mocker):
     mocked_golem_node = mocker.Mock()
 
     queue1: asyncio.Queue[str] = asyncio.Queue()
@@ -14,7 +14,7 @@ async def test_union_demand_manager_should_block_until_proposal_arrives(mocker):
     queue2: asyncio.Queue[str] = asyncio.Queue()
     mocked_get_initial_proposal2 = mocker.AsyncMock(wraps=queue2.get)
 
-    manager = UnionDemandManager(
+    manager = AggregatingDemandManager(
         mocked_golem_node,
         (
             mocked_get_initial_proposal1,
@@ -50,7 +50,9 @@ async def test_union_demand_manager_should_block_until_proposal_arrives(mocker):
     await manager.stop()
 
 
-async def test_union_demand_manager_should_remember_concurrent_proposal_for_the_next_call(mocker):
+async def test_aggregating_demand_manager_should_remember_concurrent_proposal_for_the_next_call(
+    mocker,
+):
     mocked_golem_node = mocker.Mock()
 
     queue1: asyncio.Queue[str] = asyncio.Queue()
@@ -59,7 +61,7 @@ async def test_union_demand_manager_should_remember_concurrent_proposal_for_the_
     queue2: asyncio.Queue[str] = asyncio.Queue()
     mocked_get_initial_proposal2 = mocker.AsyncMock(wraps=queue2.get)
 
-    manager = UnionDemandManager(
+    manager = AggregatingDemandManager(
         mocked_golem_node,
         (
             mocked_get_initial_proposal1,
@@ -78,8 +80,8 @@ async def test_union_demand_manager_should_remember_concurrent_proposal_for_the_
     mocked_get_initial_proposal1.mock_calls = [mocker.call()]
     mocked_get_initial_proposal2.mock_calls = [mocker.call()]
 
-    await queue2.put("b")
     await queue1.put("a")
+    await queue2.put("b")
 
     await asyncio.sleep(0.1)
 
@@ -92,7 +94,7 @@ async def test_union_demand_manager_should_remember_concurrent_proposal_for_the_
     await manager.stop()
 
 
-async def test_union_demand_manager_should_recall_func_if_not_all_are_pending(mocker):
+async def test_aggregating_demand_manager_should_recall_func_if_not_all_are_pending(mocker):
     mocked_golem_node = mocker.Mock()
 
     queue1: asyncio.Queue[str] = asyncio.Queue()
@@ -101,7 +103,7 @@ async def test_union_demand_manager_should_recall_func_if_not_all_are_pending(mo
     queue2: asyncio.Queue[str] = asyncio.Queue()
     mocked_get_initial_proposal2 = mocker.AsyncMock(wraps=queue2.get)
 
-    manager = UnionDemandManager(
+    manager = AggregatingDemandManager(
         mocked_golem_node,
         (
             mocked_get_initial_proposal1,
@@ -128,3 +130,51 @@ async def test_union_demand_manager_should_recall_func_if_not_all_are_pending(mo
     mocked_get_initial_proposal2.mock_calls = [mocker.call()]
 
     await manager.stop()
+
+
+async def test_aggregating_demand_manager_should_keep_completion_order(mocker):
+    mocked_golem_node = mocker.Mock()
+
+    queue1: asyncio.Queue[str] = asyncio.Queue()
+    mocked_get_initial_proposal1 = mocker.AsyncMock(wraps=queue1.get)
+
+    queue2: asyncio.Queue[str] = asyncio.Queue()
+    mocked_get_initial_proposal2 = mocker.AsyncMock(wraps=queue2.get)
+
+    queue3: asyncio.Queue[str] = asyncio.Queue()
+    mocked_get_initial_proposal3 = mocker.AsyncMock(wraps=queue3.get)
+
+    manager = AggregatingDemandManager(
+        mocked_golem_node,
+        (
+            mocked_get_initial_proposal1,
+            mocked_get_initial_proposal2,
+            mocked_get_initial_proposal3,
+        ),
+    )
+
+    get_task = asyncio.create_task(manager.get_initial_proposal())
+
+    done, _ = await asyncio.wait([get_task], timeout=0.1)
+    if done:
+        pytest.fail("Somehow some tasks finished too early!")
+
+    await queue3.put("c")
+
+    await asyncio.sleep(0.1)
+
+    await queue2.put("b")
+
+    await asyncio.sleep(0.1)
+
+    await queue1.put("a")
+
+    await asyncio.sleep(0.1)
+
+    assert "c" == await asyncio.wait_for(get_task, timeout=0.1)
+    assert "b" == await asyncio.wait_for(manager.get_initial_proposal(), timeout=0.1)
+    assert "a" == await asyncio.wait_for(manager.get_initial_proposal(), timeout=0.1)
+
+    mocked_get_initial_proposal1.mock_calls = [mocker.call()]
+    mocked_get_initial_proposal2.mock_calls = [mocker.call()]
+    mocked_get_initial_proposal3.mock_calls = [mocker.call()]
