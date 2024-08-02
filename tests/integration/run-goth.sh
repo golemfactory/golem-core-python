24ea4c4b3b0afbaa0565269b75e6f4a9c19fcd2d
approxit <approxit@users.noreply.github.com>
2024-08-02 13:18:34 +0200

    Release 0.7.5

--- tests/integration/run-goth.sh
+++ tests/integration/run-goth.sh
@@ -13,7 +13,7 @@ echo INSTALLING TOOLS
 .envs/goth/bin/python -m pip install --upgrade setuptools wheel
 
 echo INSTALLING DEPENDENCIES
-.envs/goth/bin/python -m pip install goth==0.15.11 pytest pytest-asyncio pexpect
+.envs/goth/bin/python -m pip install goth==0.17.0 pytest pytest-asyncio pexpect
 
 echo CREATING ASSETS
 .envs/goth/bin/python -m goth create-assets .envs/goth/assets
